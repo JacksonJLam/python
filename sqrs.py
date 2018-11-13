c4f4bad029e96dd13b7e9084935e78a916170ad0
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-13 09:41:14 -0800

    Add files via upload

---
+++ sqrs.py
@@ -0,0 +1,11 @@
+#sqrs.py
+#Created by Jackson Lambert 11-13-18
+#Prints integers 2 to 20
+
+def sqrs() :
+    for i in range(10) :
+        x = 1
+        x = x + i
+        x = x**2
+        print(x)
+sqrs()
