4aca06cd8bd9163369fd3ac46ccc01bb8fb9704a
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-13 08:51:47 -0800

    Add files via upload

---
+++ pbj.py
@@ -0,0 +1,12 @@
+#File pbj.py
+#Instructions how to make a PB & J
+#Created on 11-13-18 by Jackson Lambert
+
+def pbj() :
+    input("Once You're done with one step, hit enter to access the next")
+    input("1. First get your ingredients. Peanutbutter, Jelly and 2 slices of bread")
+    input("2. Next get a butter knife, you'll need it to spread the ingredients onto the bread")
+    input("3. Spread the peanutbutter onto one side of the bread")
+    input("4. Either spread the jelly onto the other slice of bread or onto the peanutbutter")
+    input("5. Put the two slices of bread together, whith the peanutbutter and jelly sides facing eachother")
+pbj()
