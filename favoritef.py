bcfa1ff846c7a19ceed9c1a60e9264f327848068
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-14 13:48:34 -0500

    Update favoritef.py

--- favoritef.py
+++ favoritef.py
@@ -3,6 +3,7 @@
 #Jackson Lambert 11-14-18
 
 def main() :
+    print("This is a list of my favorite foods")
     for food in ["Pizza", "Ramen", "Steak", "Tacos", "Liberal Tears"] :
         print( food )
 main()
