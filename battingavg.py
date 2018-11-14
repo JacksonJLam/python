052690e577dc7ae560f3fa974ddade0c7f9201ba
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-14 13:47:56 -0500

    Update battingavg.py

--- battingavg.py
+++ battingavg.py
@@ -3,6 +3,7 @@
 #Jackson Lambvert 11-14-18
 
 def main() :
+    print("Welcome to my batting average calculator")
     name = input("Whats the player's name?: ")
     hits = eval(input("How many hits?: "))
     at_bats = eval(input("How many at-bats?: "))
