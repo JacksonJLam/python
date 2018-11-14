3c18e8a477d29e7e9a17e9500ed2b09ac6948c14
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-14 08:55:38 -0500

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,10 @@
+#convert.py
+#A program to convert Celsius temps to Fahrenheit
+#by: Jackson Lambert 11-14-18
+def main() :
+    print("Welcome to the Celsius to Fahrenheit converter")
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9 / 5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+main ()
