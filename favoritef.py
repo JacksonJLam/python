43dd5f8970d889b08d14adc56e669f28bf82b84f
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-14 13:26:04 -0500

    Add files via upload

---
+++ favoritef.py
@@ -0,0 +1,8 @@
+#favoritef.py
+#list my favorite foods
+#Jackson Lambert 11-14-18
+
+def main() :
+    for food in ["Pizza", "Ramen", "Steak", "Tacos", "Liberal Tears"] :
+        print( food )
+main()
