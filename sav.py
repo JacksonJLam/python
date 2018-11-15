20f50ec629add432d2ea104d5718efc1e950d527
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-11-15 10:04:22 -0500

    Update sav.py

--- sav.py
+++ sav.py
@@ -4,10 +4,14 @@
 import math
 
 def main() :
+    #Welcome message
     print("Welcome, This script calculates the volume and surface area of a sphere, given it's radius.")
+   #Asks for input
     r = eval(input("Enter the radius: "))
+    #Next two lines are the equations for volume and surface area
     v = 4 / 3 * math.pi * r ** 3
     sa = 4 * math.pi * r**2
-
+    #Output
     print("The volume is", v, "and the surface area is", sa)
+#invoke
 main()
