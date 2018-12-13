db68014ab2d437bb765dbf704ebac56e4d9e0d72
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-12-13 13:03:28 -0500

    Add files via upload

---
+++ 2.1/HoP3.py
@@ -0,0 +1,18 @@
+#HoP3.py
+#Jackson Lambert 12-13-18
+#Gives out quiz grades
+
+def main() :
+    print("This script will display your quiz grade when a value 1-5 is given")
+    grade = eval(input("What was your grade?(0-100): "))
+    if grade == 100 :
+        grade = str(grade)
+        simpgrade = grade[0:2]
+        print(simpgrade)
+    else:
+        grade = str(grade)
+        simpgrade = grade[0]
+    ltrgrd = ["F","F","F","F","F","F","D","C","B","A","A"]
+    actgrad = ltrgrd[int(simpgrade)]
+    print("Your Grade is: {0}".format(actgrad))
+main()
