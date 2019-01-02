0f6f6bd3a221a164dabcea0f108de4d60ea8aaed
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2019-01-02 04:50:56 -1000

    Add files via upload

---
+++ HoP1.py
@@ -0,0 +1,17 @@
+#HoP1.py
+#Prints old macdonald with different animals
+#Jackson Lambert 1-2-19
+
+def text(animal,noise) :
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+    print("And on that farm he had a",animal,", Ee-igh, Ee-igh, Oh!")
+    print("With a ",noise,",",noise,"here and a",noise,",",noise,"there.")
+    print("Here a ",noise,", there a ",noise,", everywhere a",noise,",",noise,".")
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+def main() :
+    text("cow","moo")
+    text("chicken","bawk")
+    text("sheep","baaa")
+    text("pig","oink")
+    text("horse","neigh")
+main()
