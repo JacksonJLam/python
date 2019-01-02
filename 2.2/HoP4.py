bb87a33c3302ed7f8c9efe6ca2e1d3570515c43e
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2019-01-02 05:33:56 -1000

    Update HoP4.py

--- 2.2/HoP4.py
+++ 2.2/HoP4.py
@@ -7,9 +7,16 @@ def sumN(n) :
     val = 0
     for i in range(n + 1) :
         val = val + i
-    print(val)
+    print("The sum of the first ",n," natural numbers is",val)
 def sumNCubes(n) :
     val = 0
     for i in range(n + 1) :
         val = val + i**3
-    print(val)
+    print("The sum of the first ",n," natural numbers cubed is",val)
+def main() :
+    print("This program will find the sum of the first nth natural numbers")
+    print("It will also find the sum when cubed")
+    loop = eval(input("Enter a number: "))
+    sumN(loop)
+    sumNCubes(loop)
+main()
