efc9594773d4f48e72cd936d6450f730221e8523
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2019-01-02 05:19:23 -1000

    Add files via upload

---
+++ 2.2/HoP3.py
@@ -0,0 +1,12 @@
+#HoP3.py
+#functions to find area area and volume
+#Jackson Lambert 1-2-19
+import math
+def sphereArea(radius) :
+    area = 4 * math.pi * radius**2
+    print(area)
+def sphereVolume(radius) :
+    vol = 4 / 3 * math.pi * math.pi * radius **3
+    print(volume)
+
+#
