6b01636bfe958c02bd57484f0c708094a3d7267d
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2019-01-03 05:52:11 -1000

    Add files via upload

---
+++ 2.2/Chal4.py
@@ -0,0 +1,12 @@
+#Chal4.py
+#Countdown timer
+#Jackson Lambert 1-3-19
+import time
+times = [10,9,8,7,6,5,4,3,2,1,"LIFTOFF !!"]
+
+def main() :
+    for i in range(11) :
+        time.sleep(1)
+        print(times[i])
+main()
+    
